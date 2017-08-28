3254c1c40d103461452f07895bbc8e850cda58c5
Josh Duhay <jeduhay@yahoo.com>
2017-08-28 04:58:54 -0700

    Testing contact again

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php include_once("home.html"); ?>
+<?php include_once("home.html");
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $from = 'From: TangledDemo';
+  $to = 'jeduhay@yahoo.com';
+  $subject = 'Hello';
+  $human = $_POST['human'];
+
+  $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+  if ($_POST['submit'] && $human == '4') {
+      if (mail ($to, $subject, $body, $from)) {
+    echo '<p>Your message has been sent!</p>';
+  } else {
+    echo '<p>Something went wrong, go back and try again!</p>';
+  }
+?>
