f7ebca932576c7e162a7273f29840147c8758531
Josh Duhay <jeduhay@yahoo.com>
2017-08-28 04:53:27 -0700

    Testing contact again

--- contact.php
+++ contact.php
@@ -1,18 +1,21 @@
 <?php
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $subject = $_POST['subject'];
     $message = $_POST['message'];
-    $to = 'jeduhay@yahoo.com';
+    $from = 'From: TangledDemo';
+    $to = 'demo@tangledindesign.com';
+    $subject = 'Hello';
     $human = $_POST['human'];
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-    if ($_POST['submit']) {
-        if (mail ($to, $subject, $body)) {
-            echo '<p>Your message has been sent!</p>';
-        } else {
-            echo '<p>Something went wrong, go back and try again!</p>';
-        }
+    if ($_POST['submit'] && $human == '4') {
+        if (mail ($to, $subject, $body, $from)) {
+	    echo '<p>Your message has been sent!</p>';
+	} else {
+	    echo '<p>Something went wrong, go back and try again!</p>';
+	}
+    } else if ($_POST['submit'] && $human != '4') {
+	echo '<p>You answered the anti-spam question incorrectly!</p>';
     }
 ?>
