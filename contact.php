080197e4eb9cc027742e037e6c7536a29a7f361f
Josh Duhay <jeduhay@yahoo.com>
2017-08-28 04:40:32 -0700

    Testing contact

--- contact.php
+++ contact.php
@@ -2,7 +2,6 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $from = 'From: ';
     $to = 'jeduhay@yahoo.com';
     $subject = 'Hello';
     $human = $_POST['human'];
@@ -10,7 +9,7 @@
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
     if ($_POST['submit']) {
-        if (mail ($to, $subject, $body, $from)) {
+        if (mail ($to, $subject, $body)) {
             echo '<p>Your message has been sent!</p>';
         } else {
             echo '<p>Something went wrong, go back and try again!</p>';
